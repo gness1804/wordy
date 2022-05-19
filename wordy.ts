4924d30addac2319d0691ac0e3e1455df6658399
gness1804 <graham.nessler@gmail.com>
2022-05-19 11:39:03 -0500

    init setup

--- wordy.ts
+++ wordy.ts
@@ -1,3 +1,10 @@
-export const answer = () => {
-  throw new Error('Remove this statement and implement this function')
+export const answer = (question: string): number => {
+  const [, , firstNum, operation, operand, ...rest] = question
+    .replaceAll('divided by', 'dividedby') // make this a single word so it's easier to parse
+    .replaceAll('multiplied by', 'multipliedby') // make this a single word so it's easier to parse
+    .split(' ');
+
+  if (isNaN(parseInt(firstNum, 10))) throw new Error('Unknown operation');
+
+
 }
