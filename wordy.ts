9059e2065fa4ff990546e37e6f63c3e45ca017de
gness1804 <graham.nessler@gmail.com>
2022-05-19 11:49:19 -0500

    test for single number

--- wordy.ts
+++ wordy.ts
@@ -2,9 +2,20 @@ export const answer = (question: string): number => {
   const [, , firstNum, operation, operand, ...rest] = question
     .replaceAll('divided by', 'dividedby') // make this a single word so it's easier to parse
     .replaceAll('multiplied by', 'multipliedby') // make this a single word so it's easier to parse
+    .replace('?', '') // remove the trailing ?
     .split(' ');
 
   if (isNaN(parseInt(firstNum, 10))) throw new Error('Unknown operation');
 
+  if (!operation) {
+    // just a single number
+    return parseInt(firstNum, 10);
+  }
 
+  if (!rest.length) {
+    // there's only one operation
+
+  } else {
+    // multiple operations
+  }
 }
